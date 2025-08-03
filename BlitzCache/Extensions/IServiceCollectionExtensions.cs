c1f2843fc3762360c18586599c6075e2a70804fa
Chan <aburrio@gmail.com>
2025-08-03 13:14:09 +0200

    Improved extensions

--- BlitzCache/Extensions/IServiceCollectionExtensions.cs
+++ BlitzCache/Extensions/IServiceCollectionExtensions.cs
@@ -6,25 +6,28 @@ namespace BlitzCacheCore.Extensions
 {
     public static class IServiceCollectionExtensions
     {
-        //
-        // Summary:
-        //     /// Adds a non distributed in memory implementation of BlitzCache
-        //     to the /// Microsoft.Extensions.DependencyInjection.IServiceCollection. ///
-        //
-        // Parameters:
-        //   services:
-        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add services
-        //     to.
-        //
-        // Returns:
-        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection so that additional
-        //     calls can be chained.
-        public static IServiceCollection AddBlitzCache(this IServiceCollection services, long defaultMilliseconds = 60000)
+        /// <summary>
+        /// Adds BlitzCache as a singleton service to the dependency injection container.
+        /// </summary>
+        /// <param name="services">The service collection to add BlitzCache to.</param>
+        /// <param name="defaultMilliseconds">Default cache duration in milliseconds. Defaults to 60000 (1 minute).</param>
+        /// <returns>The service collection for method chaining.</returns>
+        public static IServiceCollection AddBlitzCache(this IServiceCollection services, long defaultMilliseconds = 60000) =>
+            AddBlitzCache(services, defaultMilliseconds, useGlobalCache: true);
+
+        /// <summary>
+        /// Adds BlitzCache as a singleton service to the dependency injection container with configuration options.
+        /// </summary>
+        /// <param name="services">The service collection to add BlitzCache to.</param>
+        /// <param name="defaultMilliseconds">Default cache duration in milliseconds.</param>
+        /// <param name="useGlobalCache">Whether to use a global shared cache (true) or instance-specific cache (false).</param>
+        /// <returns>The service collection for method chaining.</returns>
+        public static IServiceCollection AddBlitzCache(this IServiceCollection services, long defaultMilliseconds, bool useGlobalCache)
         {
             if (services == null) throw new ArgumentNullException(nameof(services));
 
             OptionsServiceCollectionExtensions.AddOptions(services);
-            ServiceCollectionDescriptorExtensions.TryAdd(services, ServiceDescriptor.Singleton<IBlitzCache>(new BlitzCache(defaultMilliseconds)));
+            ServiceCollectionDescriptorExtensions.TryAdd(services, ServiceDescriptor.Singleton<IBlitzCache>(new BlitzCache(defaultMilliseconds, useGlobalCache)));
 
             return services;
         }
